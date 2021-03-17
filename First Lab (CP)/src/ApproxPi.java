e64bae233ea162021197585cfd1b9e648b07149b
jpdiogoNOVA <jp.diogo@campus.fct.unl.pt>
2021-03-17 11:34:53 +0000

    Structure

--- First Lab (CP)/src/ApproxPi.java
+++ First Lab (CP)/src/ApproxPi.java
@@ -1,12 +1,38 @@
 
 public class ApproxPi {
+	
+	public static final int DEFAULT_POINTS = 1000;
+	public static final String TOTAL_POINTS_STAT = "Total number of points: " ;
+	public static final String POINTS_INSIDE_CIRCLE_STAT = "Points within the circle: ";
+	public static final String PI_ESTIMATION_STAT = "Pi estimation: ";
 
 	public static void main(String[] args) {
 		
+		int points_count  = 0;
+		int points_circle = 0;
+		double pi_value   = 0.0;
 		
-		int points_count = Integer.parseInt(args[0]);
+		if(args.length < 1) {
+			System.err.println("");
+			points_count = DEFAULT_POINTS;
+			
+		}
 		
-		//printStatistics();
+		points_count = Integer.parseInt(args[0]);
+		
+		
+		
+		
+		
+		printStatistics(points_count, points_circle, pi_value);
 	}
+	
+	private static void printStatistics(int points_count, int points_circle, double pi_value) {
+		System.out.println(TOTAL_POINTS_STAT + points_count);
+		System.out.println(POINTS_INSIDE_CIRCLE_STAT + points_circle);
+		System.out.println(PI_ESTIMATION_STAT + pi_value);
+		
+	}
+	
 
 }
